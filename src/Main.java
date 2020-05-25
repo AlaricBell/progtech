e72a1fcbaa39f4e792446b7eda2e3087eea86271
AlaricBell <alaric.bell@gmail.com>
2020-05-25 09:52:03 +0200

    Updating Storage model

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import model.Coffee;
 import model.Storage;
 import model.addons.*;
 import model.coffees.*;
 
 public class Main {
     public static void main(String[] args) {
+        Storage s = Storage.getStorageInstance();
+
+        s.add(new Latte());
+        s.add(new Latte());
+        s.add(new Espresso());
+        System.out.println(s.getQuantityAllByType());
     }
 }
--- src/model/Storage.java
+++ src/model/Storage.java
@@ -1,21 +1,38 @@
 package model;
 
-import model.coffees.Latte;
+import model.coffees.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Storage <T extends Coffee> {
+public class Storage {
     private static Storage firstInstance = null;
-    private List<T> coffeeStorage;
+    private static final int MAX_QUANTITY = 200;
+    private List<Coffee> coffeeStorage;
 
     private Storage() {
-        coffeeStorage = new ArrayList<T>();
+        coffeeStorage = new ArrayList<Coffee>();
     }
     public static Storage getStorageInstance() {
         if (firstInstance == null) {
             firstInstance = new Storage();
         }
         return firstInstance;
+    }
+
+    public void add(Coffee coffee) {
+        coffeeStorage.add(coffee);
+    }
+
+    public String getQuantityAllByType() {
+        String temp = "Espresso: ";
+        temp += Long.toString(coffeeStorage.stream().filter(coffee -> coffee instanceof Espresso).count()) + "\n";
+        temp += "Latte: ";
+        temp += Long.toString(coffeeStorage.stream().filter(coffee -> coffee instanceof Latte).count()) + "\n";
+        temp += "Cappuccino: ";
+        temp += Long.toString(coffeeStorage.stream().filter(coffee -> coffee instanceof Cappuccino).count()) + "\n";
+        temp += "Macchiato: ";
+        temp += Long.toString(coffeeStorage.stream().filter(coffee -> coffee instanceof Macchiato).count()) + "\n";
+        return temp;
     }
 }
