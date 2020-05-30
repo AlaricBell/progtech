eb74b431d10421d81a6a38a622f4c7abf58cb2ab
AlaricBell <alaric.bell@gmail.com>
2020-05-30 03:23:31 +0200

    Updating factory

--- src/factory/CoffeeFactory.java
+++ src/factory/CoffeeFactory.java
@@ -7,48 +7,48 @@ import model.coffees.*;
 import java.security.InvalidAlgorithmParameterException;
 
 public class CoffeeFactory {
-    public Coffee makeCoffee(String newCoffee, String flavour) throws InvalidAlgorithmParameterException {
-        if(newCoffee.toLowerCase().equals("espresso")) {
+    public static Coffee makeCoffee(String coffeeType, String flavour) throws IllegalArgumentException {
+        if(coffeeType.toLowerCase().equals("espresso")) {
             if (flavour != null) {
                 return addFlavour(new Espresso(), flavour);
             }
             return new Espresso();
         }
-        else if(newCoffee.toLowerCase().equals("latte")) {
+        else if(coffeeType.toLowerCase().equals("latte")) {
             if (flavour != null) {
                 return addFlavour(new Latte(), flavour);
             }
             return new Latte();
         }
-        else if(newCoffee.toLowerCase().equals("macchiato")) {
+        else if(coffeeType.toLowerCase().equals("macchiato")) {
             if (flavour != null) {
                 return addFlavour(new Macchiato(), flavour);
             }
             return new Macchiato();
         }
-        else if(newCoffee.toLowerCase().equals("cappuccino")) {
+        else if(coffeeType.toLowerCase().equals("cappuccino")) {
             if (flavour != null) {
                 return addFlavour(new Cappuccino(), flavour);
             }
             return new Cappuccino();
         }
         else {
-            throw new InvalidAlgorithmParameterException("Invalid parameter value in factory.make");
+            throw new IllegalArgumentException("Invalid parameter value in CoffeeFactory.makeCoffee()");
         }
     }
 
-    public Coffee addFlavour(Coffee coffee, String flavour) throws InvalidAlgorithmParameterException {
-        if(flavour.toLowerCase().equals("vanilla")) {
+    public static Coffee addFlavour(Coffee coffee, String flavour) throws IllegalArgumentException {
+        if(flavour.toLowerCase().equals("vanillaflavour")) {
             return new VanillaFlavour(coffee);
         }
-        else if(flavour.toLowerCase().equals("caramel")) {
+        else if(flavour.toLowerCase().equals("caramelflavour")) {
             return new CaramelFlavour(coffee);
         }
-        else if(flavour.toLowerCase().equals("caramel")) {
+        else if(flavour.toLowerCase().equals("chocolateflavour")) {
             return new ChocolateFlavour(coffee);
         }
         else {
-            throw new InvalidAlgorithmParameterException("Invalid parameter value in factory.make");
+            throw new IllegalArgumentException("Invalid parameter value in CoffeeFactory.addFlavour()");
         }
     }
 }
